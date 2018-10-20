1cbf12b59f2e27f0774b07a34a9cc0d02e4afe28
customer <customer@customers-MacBook-Air.local>
2018-10-20 20:43:25 +0530

    Adding find-users functionality
    Added find-users.php

--- routers/user-router.php
+++ routers/user-router.php
@@ -3,9 +3,9 @@ include '../includes/connect.php';
 	foreach ($_POST as $key => $value)
 	{
 		if(preg_match("/[0-9]+_role/",$key)){
-		//	$key = strtok($key, '_');
-		//	$sql = "UPDATE users SET role = '$value' WHERE id = $key;";
-			//$con->query($sql);
+			$key = strtok($key, '_');
+			$sql = "UPDATE users SET role = '$value' WHERE id = $key;";
+			$con->query($sql);
 		}
 		if(preg_match("/[0-9]+_verified/",$key)){
 			$key = strtok($key, '_');
